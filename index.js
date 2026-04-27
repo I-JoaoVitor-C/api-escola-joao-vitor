a636c4281de4ad733612e13011eb8606be2566ae
I-JoaoVitor-C <ferreira.silva.joao2910@escola.pr.gov.br>
2026-04-26 21:47:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const PORT = 3000;
 
 // Middlewares obrigatórios
-app.use(express.json()); // Permite receber dados no corpo das requisições POST e PUT [cite: 77]
-app.use(express.static('public')); // Serve os arquivos estáticos (Front-End) da pasta public/ [cite: 28, 74]
+app.use(express.json()); // Permite receber dados no corpo das requisições POST e PUT 
+app.use(express.static('public')); // Serve os arquivos estáticos (Front-End) da pasta public/ 
 
 // Rota de teste
 app.get('/api/status', (req, res) => {
